ff6f4c16144e914aeaa2effdbe95a53999947fae
Ralph <ralphstevendc@gmail.com>
2020-02-17 00:53:32 -0800

    Fixed the login and register functions to make sure they connect to MongoDB

--- back_end/web/app.py
+++ back_end/web/app.py
@@ -5,24 +5,23 @@ import bcrypt
 from flask_jwt_extended import JWTManager 
 from flask_jwt_extended import create_access_token
 
-app = Flask(__name__)
-api = Api(app)
-
-client = MongoClient('mongodb://localhost:27017')
-db = client.NewsAggregatorDB
-users = db['Users']
-
-app.config['JWT_SECRET_KEY'] = 'secret'
-
-jwt = JWTManager(app)
-
-
 # For Web Scraping
 import requests
 from bs4 import BeautifulSoup
 
+app = Flask(__name__)
+api = Api(app)
+
+client = MongoClient("mongodb://db:27017")
+db = client.UsersManagementDB
+users = db["Users"]
+
+app.config["JWT_SECRET_KEY"] = "secret"
+
+jwt = JWTManager(app)
+
 def UserExist(username):
-    if users.find({'Username':username}).count() == 0:
+    if users.count_documents({"Username":username}) == 0:
         return False
     else:
         return True
@@ -33,21 +32,9 @@ class Register(Resource):
         postedData = request.get_json()
 
         #Get the data
-        first_name = postedData['first_name']
-        last_name = postedData['last_name']
-        username = postedData['username']
-        password = postedData['password'] #'123xyz'
-        urls = postedData['urls']
+        username = postedData["username"]
+        password = postedData["password"] #"123xyz"
 
-        retObj = {
-            'first_name': first_name,
-            'last_name': last_name,
-            'username': username,
-            'password': password,
-            'urls': urls
-        }
-
-        
         if UserExist(username):
             retJson = {
                 'status':301,
@@ -59,38 +46,35 @@ class Register(Resource):
 
         #Store username and pw into the database
         users.insert_one({
-            'first_name': first_name,
-            'last_name': last_name,
-            'username': username,
-            'password': hashed_pw,
-            'urls': urls
+            "Username": username,
+            "Password": hashed_pw
         })
 
         retJson = {
-            'status': 200,
-            'msg': retObj
+            "status": 200,
+            "msg": "Success"
         }
         return jsonify(retJson)
 
 class Login(Resource):
     def post(self):
         postedData = request.get_json()
-        username = postedData['username']
-        password = postedData['password'] #'123xyz'
+        username = postedData["username"]
+        password = postedData["password"] #"123xyz"
 
-        result = ''
+        result = ""
 
-        foundUsername = users.find_one({'username': username})
+        foundUsername = users.find_one({"username": username})
         hashed_pw = users.find({ "username":username })[0]["password"]
 
         if foundUsername:
-            if bcrypt.hashpw(password.encode('utf8'), hashed_pw) == hashed_pw:
+            if bcrypt.hashpw(password.encode("utf8"), hashed_pw) == hashed_pw:
                 access_token = create_access_token(identity = {
-                    'first_name': foundUsername['first_name'],
-                    'last_name': foundUsername['last_name'],
-                    'username': foundUsername['username']
+                    "first_name": foundUsername["first_name"],
+                    "last_name": foundUsername["last_name"],
+                    "username": foundUsername["username"]
                 })
-                result = jsonify({'token':access_token})
+                result = jsonify({"token":access_token})
             else:
                 result = jsonify({"error":"Invalid username and password"})
         else:
@@ -100,65 +84,67 @@ class Login(Resource):
 
 class WebScraperYahoo(Resource):
     def get(self):
-        URL = 'https://www.yahoo.com'
+        URL = "https://www.yahoo.com"
         page = requests.get(URL)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        results = soup.find(id='applet_p_50000313')
-        elems = results.find_all('li', class_='ntk-item W(1/5) Whs(n) Va(t) D(ib) Lts(0) Bdend(none) Pos(r)')
+        soup = BeautifulSoup(page.content, "html.parser")
+        results = soup.find(id="applet_p_50000313")
+        elems = results.find_all("li", class_="ntk-item W(1/5) Whs(n) Va(t) D(ib) Lts(0) Bdend(none) Pos(r)")
         news_links = dict()
         for news in elems:
-            links = news.find('a', class_='Pos(r) D(b) Mend(9px) C($link) C($m_blue):h C($m_blue):f O(n):f Op(0.9):h Op(0.9):f Td(n) W(a) ntk-footer-link js-content-viewer rapidnofollow wafer-caas')['href']
-            title = news.find('h3', class_='Mx(0) Mb(0) Mt(4px) Fz(12px) LineClamp(2,2.6em) LineClamp(3,4em)!--md1100 js-stream-content-link:f_Td(u) T(70%) js-stream-content-link:f_Td(n)! Start(2px) Td(u):h')
+            links = news.find("a", class_="Pos(r) D(b) Mend(9px) C($link) C($m_blue):h C($m_blue):f O(n):f Op(0.9):h Op(0.9):f Td(n) W(a) ntk-footer-link js-content-viewer rapidnofollow wafer-caas")["href"]
+            title = news.find("h3", class_="Mx(0) Mb(0) Mt(4px) Fz(12px) LineClamp(2,2.6em) LineClamp(3,4em)!--md1100 js-stream-content-link:f_Td(u) T(70%) js-stream-content-link:f_Td(n)! Start(2px) Td(u):h")
             if None in (title, links):
                 continue
-            print("Title: " + title.text.strip(), end='\n' * 2)
-            print(links, end='\n' * 2)
+            print("Title: " + title.text.strip(), end="\n" * 2)
+            print(links, end="\n" * 2)
             news_links[title.text.strip()] = "www.yahoo.com" + links
         return news_links
 
 class WebScraperMedicalToday(Resource):
     def get(self):
-        URL = 'https://www.medicalnewstoday.com/'
+        URL = "https://www.medicalnewstoday.com/"
         page = requests.get(URL)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        results = soup.find('div', class_='css-stl7tm')
-        elems = results.find_all('div', class_='css-8sm3l3')
+        soup = BeautifulSoup(page.content, "html.parser")
+        results = soup.find("div", class_="css-stl7tm")
+        elems = results.find_all("div", class_="css-8sm3l3")
         news_links = dict()
         for news in elems:
-            title = news.find('a', class_='css-ni2lnp')
-            links = news.find('a', class_='css-ni2lnp')['href']
+            title = news.find("a", class_="css-ni2lnp")
+            links = news.find("a", class_="css-ni2lnp")["href"]
             if None in (title, links):
                 continue
-            print("Title: " + title.text.strip(), end='\n' * 2)
-            print(links, end='\n' * 2)
+            print("Title: " + title.text.strip(), end="\n" * 2)
+            print(links, end="\n" * 2)
             news_links[title.text.strip()] = "www.medicalnewstoday.com" + links
         return news_links
 
 class WebScraperBuzzFeedBooks(Resource):
     def get(self):
-        URL = 'https://www.buzzfeednews.com/section/books'
+        URL = "https://www.buzzfeednews.com/section/books"
         page = requests.get(URL)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        results = soup.find('div', class_='grid-layout-wrapper content-column')
-        elems = results.find_all('span', class_='newsblock-story-card__info xs-pr1 xs-block')
+        soup = BeautifulSoup(page.content, "html.parser")
+        results = soup.find("div", class_="grid-layout-wrapper content-column")
+        elems = results.find_all("span", class_="newsblock-story-card__info xs-pr1 xs-block")
         news_links = dict()
         for news in elems:
-            links = news.find('a', class_='newsblock-story-card__link xs-flex')['href']
-            title = news.find('a', class_='newsblock-story-card__link xs-flex')
+            links = news.find("a", class_="newsblock-story-card__link xs-flex")["href"]
+            title = news.find("a", class_="newsblock-story-card__link xs-flex")
             if None in (title, links):
                 continue
-            print("Title: " + title.text.strip(), end='\n' * 2)
-            print(links, end='\n' * 2)
+            print("Title: " + title.text.strip(), end="\n" * 2)
+            print(links, end="\n" * 2)
             news_links[title.text.strip()] = "www.yahoo.com" + links
         return news_links
 
-api.add_resource(Register, '/api/register')
-api.add_resource(Login, '/api/login')
-api.add_resource(WebScraperYahoo, '/api/yahoo/scrape')
-api.add_resource(WebScraperMedicalToday, '/api/medical/scrape')
-api.add_resource(WebScraperBuzzFeedBooks, '/api/books/scrape')
+api.add_resource(Register, "/api/register")
+api.add_resource(Login, "/api/login")
+api.add_resource(WebScraperYahoo, "/api/yahoo/scrape")
+api.add_resource(WebScraperMedicalToday, "/api/medical/scrape")
+api.add_resource(WebScraperBuzzFeedBooks, "/api/books/scrape")
 
 
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0')
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
+
+
